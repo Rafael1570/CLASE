4a2cb86ca1e68cf80b7c14f2f471f5a2fc9cf0cc
Rafael1570 <rafaelvegalibrero@gmail.com>
2025-11-28 11:02:41 +0100

    Despues de examen

---
+++ Entorno de desarollo/Ejemplo3.java
@@ -0,0 +1,11 @@
+public class Ejemplo3 { 
+  public static void main(String[] args) { 
+    int numero = 7; 
+    if (numero % 2 == 0) {
+      System.out.println("El número es par");
+    } else {
+      System.out.println("El número es impar");
+    }
+    System.out.println("Fin del programa");
+  }
+}
