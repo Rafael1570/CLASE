b5612ff934aa83629c0996d99b7dc4d2f270c12f
Rafael1570 <rafaelvegalibrero@gmail.com>
2026-04-29 08:22:32 +0200

    29/ abril

---
+++ Entorno de desarollo/Practica4GitRafaelVegaLibrero/lib/Ejemplo1.java
@@ -0,0 +1,9 @@
+public class Ejemplo1 { 
+  public static void main(String[] args) 
+  { 
+    int a = 3; 
+    int b = 7; 
+    int c = a + b;
+    System.out.println("Resultado: " + c);
+  }
+}
--- Entorno de desarollo/Practica4GitRafaelVegaLibrero/lib/Ejemplo2.java
+++ Entorno de desarollo/Practica4GitRafaelVegaLibrero/lib/Ejemplo2.java
@@ -1,4 +1,4 @@
-public class Ejemplo3 { 
+public class Ejemplo2 { 
   public static void main(String[] args) { 
     int numero = 7; 
     if (numero % 2 == 0) {
